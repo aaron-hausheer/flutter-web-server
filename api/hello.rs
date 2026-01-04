81ffffff2e29fa1280d3d600e8431255a95f41a6
Aaron Hausheer <aahaus@gmx.ch>
2026-01-04 18:19:04 +0100

    Fix vercel stuff

---
+++ api/hello.rs
@@ -0,0 +1,10 @@
+use vercel_runtime::{run, Body, Error, Request, Response};
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    run(handler).await
+}
+
+async fn handler(_req: Request) -> Result<Response<Body>, Error> {
+    Ok(Response::builder().status(200).body(Body::Text("ok".into()))?)
+}
